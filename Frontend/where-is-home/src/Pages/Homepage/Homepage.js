2e0d06b74bdae4ca055cec9ca80b41df6f0fb16d
Vasco_Faria <vascomfaria@ua.pt>
2024-03-17 00:01:30 +0000

    Home Quote added

--- Frontend/where-is-home/src/Pages/Homepage/Homepage.js
+++ Frontend/where-is-home/src/Pages/Homepage/Homepage.js
@@ -1,12 +1,23 @@
 import React from "react";
 import './Homepage.css';
+import {useTranslation} from "react-i18next";
+import videobackground from './../../Assets/Video/videobackground.mp4';
 
 
 function Homepage( {darkMode}) {
 
+    const {t} = useTranslation("common");
+
 return (
     <div className={`home-section ${darkMode ? 'dark-mode' : 'light-mode'}`}>
-
+        <div className="video-container">
+                        <video autoPlay muted loop className="video" disablePictureInPicture controlsList="nodownload">
+                            <source src={videobackground} type="video/mp4" />
+                        </video>
+        </div>
+        <h2 className='hometitle'>
+               {t('QuoteHome')}!
+        </h2>
     </div>
 );
 

799c4dcf04ab145789b647597b1c4bff2a8e8413
Vasco_Faria <vascomfaria@ua.pt>
2024-03-18 22:13:34 +0000

    Changes

--- Frontend/where-is-home/src/App.js
+++ Frontend/where-is-home/src/App.js
@@ -2,49 +2,42 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Homepage from './Pages/Homepage/Homepage';
 import Loader from './Components/Loader/Loader';
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './App.css';
 import Login_register from './Pages/Login-register/Login_register';
 
 function App() {
-
   const [darkMode, setDarkMode] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     setDarkMode(prefersDarkMode);
-}, []);
+  }, []);
 
-const toggleDarkMode = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const toggleDarkMode = () => {
     setDarkMode(!darkMode);
     document.documentElement.classList.toggle('dark-mode', !darkMode);
-};
-
-useEffect(() => {
-  const timer = setTimeout(() => {
-      setLoading(false);
-  }, 2000);
-
-  return () => clearTimeout(timer);
-}, []);
-
-
+  };
 
   return (
     <div className={`App ${darkMode ? 'dark-mode' : 'light-mode'}`}>
-      <Loader visible={loading} /> 
-       {!loading && (
-        <>
-        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-        <BrowserRouter>
-                          <Routes>
-                          <Route exact path="/" element={<Homepage darkMode={darkMode} />} />
-                          <Route exact path="/login" element={<Login_register  darkMode={darkMode} />}/>
-                          </Routes>
-          </BrowserRouter>
-        </>
-        )}
+      <Loader visible={loading} />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage darkMode={darkMode} />} />
+          <Route exact path="/login" element={<Login_register  darkMode={darkMode} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

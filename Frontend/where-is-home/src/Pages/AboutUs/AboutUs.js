4b18fcf8c79f6c43e7f545878c4fbf09f296cb59
Vasco_Faria <vascomfaria@ua.pt>
2024-03-18 23:41:46 +0000

    Init About Us component

---
+++ Frontend/where-is-home/src/Pages/AboutUs/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs ({darkMode}) {
+        return (
+            <div className={`AboutUs ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+                
+            </div>
+        );
+    
+}
+
+export default AboutUs;

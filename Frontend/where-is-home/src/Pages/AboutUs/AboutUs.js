ee9af82e19524edb95f67987de96589d7edd1139
Vasco_Faria <vascomfaria@ua.pt>
2024-03-19 09:30:17 +0000

    Structure init

--- Frontend/where-is-home/src/Pages/AboutUs/AboutUs.js
+++ Frontend/where-is-home/src/Pages/AboutUs/AboutUs.js
@@ -4,7 +4,15 @@ import './AboutUs.css';
 function AboutUs ({darkMode}) {
         return (
             <div className={`AboutUs ${darkMode ? 'dark-mode' : 'light-mode'}`}>
-                
+                <div className='backline'></div>
+                <div className='aboutus-container'>
+                    <div className='aboutus-text-container'>
+
+                    </div>
+                    <div className='about-us-video'>
+                        <div></div>
+                    </div>
+                </div>
             </div>
         );
     

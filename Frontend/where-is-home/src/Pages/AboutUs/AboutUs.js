903a29916b67c91ab21ce23595bbfb08d5594cc2
Vasco_Faria <vascomfaria@ua.pt>
2024-03-19 09:50:45 +0000

    Text and video

--- Frontend/where-is-home/src/Pages/AboutUs/AboutUs.js
+++ Frontend/where-is-home/src/Pages/AboutUs/AboutUs.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './AboutUs.css';
+import {useTranslation} from "react-i18next";
+
 
 function AboutUs ({darkMode}) {
+    const {t} = useTranslation("common");
         return (
             <div className={`AboutUs ${darkMode ? 'dark-mode' : 'light-mode'}`}>
                 <div className='backline'></div>
                 <div className='aboutus-container'>
                     <div className='aboutus-text-container'>
 
+                    <h2>About Us</h2>
+
+                    <div className='text'>
+                        <p>Texto</p>
+                    </div>
+
                     </div>
                     <div className='about-us-video'>
                         <div></div>
                     </div>
                 </div>
-            </div>
+            </div>      
         );
     
 }

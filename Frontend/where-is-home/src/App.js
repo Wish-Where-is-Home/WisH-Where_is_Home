56ba4bb56444401fef66d124b81f314a22876eda
Tiago Cruz <tiagofcruz78@ua.pt>
2024-04-10 15:33:59 +0100

    metrics

--- Frontend/where-is-home/src/App.js
+++ Frontend/where-is-home/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login_register from './Pages/Login-register/Login_register';
 import AboutUs from './Pages/AboutUs/AboutUs';
+import InitMetrics from './Pages/Metrics/InitMetrics';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -38,6 +39,7 @@ function App() {
           <Route exact path="/" element={<Homepage darkMode={darkMode} />} />
           <Route exact path="/aboutus" element={<AboutUs darkMode={darkMode}/>}/>
           <Route exact path="/login" element={<Login_register  darkMode={darkMode} />} />
+          <Route exact path="/initMetrics" element={<InitMetrics darkMode={darkMode} />} />
         </Routes>
       </Router>
     </div>

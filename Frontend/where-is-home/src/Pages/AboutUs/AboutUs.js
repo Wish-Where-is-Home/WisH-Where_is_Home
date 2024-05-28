579122e444363ad2655e07739a0b0377a24f2b69
Vasco_Faria <vascomfaria@ua.pt>
2024-05-28 16:19:45 +0100

    video

--- Frontend/where-is-home/src/Pages/AboutUs/AboutUs.js
+++ Frontend/where-is-home/src/Pages/AboutUs/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './AboutUs.css';
 import {useTranslation} from "react-i18next";
+import video from './../../Assets/Video/promovideo.mp4';
 
 
 function AboutUs ({darkMode}) {
@@ -19,11 +20,15 @@ function AboutUs ({darkMode}) {
 
                     </div>
                     <div className='about-us-video'>
-                        <div></div>
+                        <div>
+                        <video width="100%" height="100%" className="video2" autoPlay loop muted disablePictureInPicture controlsList="nodownload">                            <source src={video} type="video/mp4"/>
+                            Seu navegador não suporta a tag de vídeo.
+                        </video>
+                        </div>
                     </div>
                 </div>
             </div>      
-        );
+            );
     
 }
 

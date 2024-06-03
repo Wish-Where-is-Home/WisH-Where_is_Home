aa6a3e7affa744ef15771275194ad022b6fc6ab5
Vasco_Faria <vascomfaria@ua.pt>
2024-06-03 11:03:19 +0100

    fix price property ownerpage

--- Frontend/where-is-home/src/Components/OwnerPropDetails/OwnerPropDetails.js
+++ Frontend/where-is-home/src/Components/OwnerPropDetails/OwnerPropDetails.js
@@ -141,7 +141,7 @@ function OwnerPropDetails({ property, allRooms, token, onRoomAvailabilityChange,
                                 <p><span>Room {index + 1}:</span> </p>
                                 <p><span>Included Expenses:</span> {room.despesas_incluidas}</p>
                                 <p><span>Private Bathroom:</span> {room.wc_privado ? 'Yes' : 'No'}</p>
-                                <p><span>Price:</span> {room.preco} €</p>
+                                <p><span>Price:</span> {room.preco_mes} €</p>
                                 <p>
                                     <span>Available:</span> {room.disponivel ? 'Yes' : 'No'}
                                     <button onClick={() => handleRoomAvailabilityChange(room.id, room.disponivel)}>
